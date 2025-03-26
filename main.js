253f0ada420d6b11aca149450a98f80ed583760c
She7ataJr <She7ataJr@gmail.com>
2025-03-26 23:44:27 +0200

    search fn added

--- main.js
+++ main.js
@@ -2,9 +2,17 @@ let productName = document.getElementById('proName');
 let seller = document.getElementById('seller')
 let price = document.getElementById('price')
 const addBtn=document.getElementById('addBtn')
+const delBtn=document.getElementById('delBtn')
+const searchInput=document.getElementsByClassName('search-text')
 // adding func
 let products=[];
 
+if( localStorage.getItem('myItems')!= null){
+    products = JSON.parse(localStorage.getItem('myItems'))
+    display();
+}else{
+    products=[];
+}
 function add(){
     
     const item={
@@ -14,17 +22,61 @@ function add(){
         itemPrice:price.value
     }
     products.push(item)
+    localStorage.setItem("myItems",JSON.stringify(products))
+    clearForm()
     console.log(products);
 }
 
 function display(){
     var itemcontainer=''
     for (let i = 0; i < products.length; i++) {
-        itemcontainer+=`<tr><td>${products[i].name}</td><td>${products[i].sellerName}</td><td>${products[i].itemPrice}</td></tr>`
+        itemcontainer+=`<tr><td>${products[i].name}</td>
+        <td>${products[i].sellerName}</td>
+        <td>${products[i].itemPrice}</td>
+        <td> <button type="button" onclick="deleteItem(${i})" class="btn btn-outline-danger">Delete</button></td>
+        <td> <button type="button" class="btn btn-outline-warning">Update</button></td>
+        </tr>`
     }
     document.getElementById('tBody').innerHTML=itemcontainer
 }
 addBtn.onclick = function(){
     add();
     display();
+}
+
+function clearForm(){
+    // first idea
+    // productName.value=''
+    // seller.value=''
+    // price.value=''
+    // enhanced idea 
+    let inputs = document.getElementsByClassName('form-control')
+    for (i=0; i<inputs.length; i++){
+        inputs[i].value=''
+    }
+}
+// Local storage 
+function deleteItem(index){
+    products.splice(index,1)
+    console.log(products);
+    
+    display()
+}
+
+function search(searchtxt){
+
+    var itemcontainer=''
+    for (let i = 0; i < products.length; i++) {
+        if(products[i].name.toLowerCase().includes(searchtxt.toLowerCase())){
+            itemcontainer+=`<tr><td>${products[i].name}</td>
+            <td>${products[i].sellerName}</td>
+            <td>${products[i].itemPrice}</td>
+            <td> <button type="button" onclick="deleteItem(${i})" class="btn btn-outline-danger">Delete</button></td>
+            <td> <button type="button" class="btn btn-outline-warning">Update</button></td>
+            </tr>`
+        }
+       
+    }
+    document.getElementById('tBody').innerHTML=itemcontainer
+
 }
